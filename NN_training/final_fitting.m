f27b358d5d6158292e78a070fc8fb99184b014a7
Zeeshan <azeeshan@andrew.cmu.edu>
2017-10-09 15:31:10 -0400

    Added Rsq criterion

--- NN_training/final_fitting.m
+++ NN_training/final_fitting.m
@@ -4,6 +4,14 @@ clear all
 %close all
 %clc
 tic;
+
+mycluster = parcluster('local')
+%mycluster.JobStorageLocation = strcat('/home/azeeshan/.matlab','/', getenv('SLURM_JOB_ID'))
+%parpool(mycluster, str2num(getenv('SLURM_CPUS_ON_NODE')))
+mycluster.NumWorkers = 24
+parpool(mycluster)
+saveProfile(mycluster)
+
 lattice = fileread('lattice-type.txt');
 load(strcat(lattice,'_results.mat'))
 load(fullfile('..','data-gen',strcat(lattice,'-non-training-data.mat')))
@@ -12,8 +20,8 @@ load(fullfile('..','data-gen',strcat(lattice,'-data.mat'))) % xdata and ydata
 load(fullfile('..','Linear',strcat('features_',lattice,'.mat')))
 
 %% User input data
-X_mat = xdata;
-coeffs = ydata;
+X_mat = xdata(1:end-floor(0.1*size(xdata,1)),:);
+coeffs = ydata(1:end-floor(0.1*size(xdata,1)),:);
 num_coeffs = size(coeffs,2);
 cubic_nt = xntdata;
 
@@ -23,9 +31,9 @@ test_err_complete = cell(num_coeffs,1);
 index_out_coeffs = cell(num_coeffs,1);
 
 % cubic_nt = cubic_nt';  % Just converting the array to transpose %Now this becomes 692X13
-parfor coeff_num = 1:1:num_coeffs
+for coeff_num = 1:1:num_coeffs
     coeff_num
-    hidden_layer_size = hidden_layer_av(coeff_num); % you can provide a row vector which can represent if we need multiple hidden layers
+    hidden_layer_size = hidden_layer_av(coeff_num) %you can provide a row vector which can represent if we need multiple hidden layers
     % pre processing normalization
     feature_arr = feature_list{coeff_num};
     
@@ -51,16 +59,19 @@ parfor coeff_num = 1:1:num_coeffs
     XTRAIN = x;
     ytrain = t;
     %XTEST = XTEST';
-    
-    val_perf_reqd=0.005;
-    tr_perf_reqd = 0.001;
+
+    val_perf_reqd=0.01;
+    tr_perf_reqd = 0.01;
     reg_tr_reqd=0.9;
+Rsq_val_reqd = 0.8;
+Rsq_tr_reqd = 0.9; 
     max_index = 1000;
     ind_out = zeros(sample_test, 1);
     net_storage = cell(sample_test,1);
     tr_storage = cell(sample_test,1);
     
-    for random_weights = 1:1:sample_test
+    parfor random_weights = 1:1:sample_test
+        random_weights
         val_min = 1;
         tr_perf_min = 1;
         net_min = [];
@@ -74,60 +85,31 @@ parfor coeff_num = 1:1:num_coeffs
         net.divideParam.trainRatio = 80/100;
         net.divideParam.valRatio = 20/100;
         net.divideParam.testRatio = 0/100;
-        net.performParam.regularization = 0.01;
+        %        net.performParam.regularization = 0.01;
         net.trainParam.max_fail = 15;
         
         while((bool_var))
             init(net);
             [net,tr] = train(net,XTRAIN,ytrain);
             out_train = net(XTRAIN);
-            %e = gsubtract(XTRAIN,ytrain);
-            %performance = perform(net,ytrain,out_train);
-            if(size(ytrain) ~= size(out_train))
-                ytrain = ytrain';
-            end
             trTarg = ytrain(tr.trainInd);
             trOut = out_train(tr.trainInd);
-            
-            %vOut = out_train(tr.valInd);
-            %vTarg = ytrain(tr.valInd);
-            
-            %testOut = out_train(tr.testInd);
-            %testTarg = ytrain(tr.testInd);
-            
-            %train_err_val = mean((trTarg-trOut).^2);
-            %test_err_val = mean((testTarg-testOut).^2);
-            %val_err_val  = mean((vTarg-vOut).^2);
             [reg_tr,~,~] = regression(trTarg, trOut);
+Rsq_tr = 1 - sum((ytrain(tr.trainInd) - out_train(tr.trainInd)).^2)/sum((ytrain(tr.trainInd) - mean(ytrain(tr.trainInd))).^2);
+Rsq_val = 1 - sum((ytrain(tr.valInd) - out_train(tr.valInd)).^2)/sum((ytrain(tr.valInd) - mean(ytrain(tr.valInd))).^2);
             %[reg_val,~,~] = regression(vTarg, vOut);
             %[reg_test,~,~] = regression(testTarg, testOut)
             val_perf = mse(net,ytrain(tr.valInd),out_train(tr.valInd));
             tr_perf = mse(net,ytrain(tr.trainInd),out_train(tr.trainInd));
-            bool_var = (val_perf>val_perf_reqd)||(tr_perf>tr_perf_reqd)%(reg_tr<reg_tr_reqd); % Training parameter
-            %val_perf
-            %tr_perf
-            
+            %bool_var = (val_perf>val_perf_reqd)||(reg_tr<reg_tr_reqd); % (tr_perf>tr_perf_reqd) Training parameter
+	    bool_var = (Rsq_val < Rsq_val_reqd) || (Rsq_tr < Rsq_tr_reqd)
             if val_perf<val_min %&& tr_perf<tr_perf_min
                 val_min = val_perf;
                 tr_perf_min = tr_perf;
                 net_min = net;
                 tr_min = tr;
-                %             t_train_min = ytrain(tr.trainInd);
-                %             t_val_min = ytrain(tr.valInd);
-                %             t_test_min = ytrain(tr.testInd);
-                %             x_train_min = XTRAIN(:,tr.trainInd);
-                %             x_val_min = XTRAIN(:,tr.valInd);
-                %             x_test_min = XTRAIN(:,tr.testInd);
             end
             if index_watch>=max_index % this is just for the case when the iterations do not converge even after 1000 runs ....
-                %             trOut = net_min(x_train_min);
-                %             vOut = net_min(x_val_min);
-                %             testOut = net_min(x_test_min);
-                %             trTarg = t_train_min;
-                %             vTarg = t_val_min;
-                %             testTarg = t_test_min;
-                %             net = net_min;
-                %             out_train = net(XTRAIN);
                 ind_out(random_weights) = 1;
                 break;
             end
@@ -137,16 +119,7 @@ parfor coeff_num = 1:1:num_coeffs
         end
         net_storage{random_weights} = net_min;
         tr_storage{random_weights} = tr_min;
-        random_weights
-        %test_k_fold = net(XTEST);
-        %t_test_total_Targ = cat(2,testTarg,t_test);
-        %t_test_total_Out = cat(2,testOut,test_k_fold);
         
-        %train_err(random_weights) = mean((trTarg - trOut).^2);
-        %test_err(random_weights) = mean((t_test_total_Targ - t_test_total_Out).^2);
-        %val_err(random_weights) = mean((vTarg - vOut).^2);
-        %tot_err(random_weights) = mean((ytrain-out_train).^2);
-        %net_collection{random_weights} = net;
     end
     net_storage_complete{coeff_num} = net_storage;
     tr_storage_complete{coeff_num} = tr_storage;
@@ -155,6 +128,3 @@ parfor coeff_num = 1:1:num_coeffs
 end
 toc;
 save(strcat(lattice,'_final_results.mat'),'net_storage_complete','index_out_coeffs','tr_storage_complete')
-%save('cubic_net_storage_complete24Jul.mat','net_storage_complete');
-%save('cubic_test_err_complete24Jul.mat','test_err_complete');
-%save('cubic_index_partial_trained24Jul.mat','index_out_coeffs');

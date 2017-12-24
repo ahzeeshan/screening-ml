ce91586d3248fee575ad4616eb282b0f1149fd07
Zeeshan Ahmad <azeeshan@andrew.cmu.edu>
2017-12-23 20:15:50 -0500

    Added fin with layer with br reg

---
+++ NN_training/final_fitting_brlayer.m
@@ -0,0 +1,148 @@
+B1;95;0c%This code will take the numner of neurons and do the calcs
+%%
+clear all
+%close all
+%clc
+tic;
+
+mycluster = parcluster('local')
+mycluster.JobStorageLocation = strcat(getenv('SCRATCH'),'/.matlab/', getenv('SLURM_JOB_ID'))
+mycluster.NumWorkers = str2num(getenv('SLURM_NTASKS'))
+parpool(mycluster, mycluster.NumWorkers)
+saveProfile(mycluster)
+
+lattice = fileread('lattice-type.txt');
+load(strcat(lattice,'_results.mat'))
+load(fullfile('..','data-gen',strcat(lattice,'-non-training-data.mat')))
+
+load(fullfile('..','data-gen',strcat(lattice,'-data-posd.mat'))) % xdata and ydata
+load(fullfile('..','Linear',strcat('features_',lattice,'.mat')))
+
+%% User input data
+X_mat = xdata(trainIndglob,:);
+coeffs = ydata(trainIndglob,:);
+num_coeffs = size(coeffs,2);
+cubic_nt = xntdata;
+
+net_storage_complete = cell(num_coeffs,1);
+tr_storage_complete = cell(num_coeffs,1);
+test_err_complete = cell(num_coeffs,1);
+index_out_coeffs = cell(num_coeffs,1);
+
+% cubic_nt = cubic_nt';  % Just converting the array to transpose %Now this becomes 692X13
+for coeff_num = 1:num_coeffs
+    coeff_num
+    
+    %[M,I]=min(mse_size{coeff_num}(:));
+    %[aa,bb]=ind2sub(size(mse_size{coeff_num}),I) ;
+    %hidden_layer_size = bb 
+    hidden_layer_size = hidden_layer_av(coeff_num); %you can provide a row vector which can represent if we need multiple hidden layers
+    % pre processing normalization
+    feature_arr = feature_list{coeff_num};
+    
+    X1 = X_mat(:,feature_arr);
+    non_training_yy = cubic_nt(:,feature_arr)';
+    
+    yy = X1'; %Doing transpose to feed into Neural network
+    
+    %Mapminmax starts below to map x from [-1,1]
+    
+    [x, tot_inp_recover] = mapminmax(yy);
+    % tot_inp is our favorable output and tot_inp_recover is the structure by
+    % which we can apply that transformation to other values as well and can
+    % also get the original value of tot_inp.
+    non_training = mapminmax('apply',non_training_yy,tot_inp_recover);
+    
+    t_yy = coeffs(:,coeff_num)'; % t here refers to the target in ANNs
+    [t, t_recover] = mapminmax(t_yy); %Mapping the coefficients as well in [-1,1]
+    Y = t';
+    %% Looping
+    sample_test = 1000;
+    trainFcn = 'trainbr';
+    XTRAIN = x;
+    ytrain = t;
+    %XTEST = XTEST';
+    
+    val_perf_reqd=0.01;
+    tr_perf_reqd = 0.01;
+    reg_tr_reqd=0.9;
+    Rsq_val_reqd = -inf; Rsq_tt_reqd = 0.7;
+    Rsq_tr_reqd = 0.85;
+    max_index = 1000;
+    ind_out = zeros(sample_test, 1);
+    net_storage = cell(sample_test,1);
+    tr_storage = cell(sample_test,1);
+    
+    while ( hidden_layer_size==hidden_layer_av(coeff_num) ) || ( length(find(ind_out==0))<10 && (hidden_layer_size <= length(feature_list{coeff_num})) ) 
+        hidden_layer_size
+        parfor random_weights = 1:sample_test
+            random_weights
+            tt_min = 1000;
+            tr_perf_min = 1000;
+            net_min = [];tr_min = [];
+            bool_var = 1;
+            index_watch = 1;
+            
+            net = fitnet(hidden_layer_size,trainFcn);
+            net = configure(net,XTRAIN,ytrain);
+            net.trainParam.showWindow = 0;
+            net.divideParam.trainRatio = 85/100;
+            net.divideParam.valRatio = 15/100;
+            net.divideParam.testRatio = 0/100;
+            %if hidden_layer_size>=(length(ytrain)* ...
+            %                       net.divideParam.trainRatio - ...
+            %                       1)/(length(feature_list{coeff_num})+2)
+            %    net.performParam.regularization = 0.1;
+            %end
+            %net.performParam.regularization = 0.01;
+            net.trainParam.max_fail = 15;
+            
+            ind_out(random_weights) = 0; % Flag for whether it found within 1000 iters 0<->yes
+            
+            while(bool_var)
+                init(net);
+                [net,tr] = train(net,XTRAIN,ytrain);
+                out_train = net(XTRAIN);
+                trTarg = ytrain(tr.trainInd);
+                trOut = out_train(tr.trainInd);
+                %[reg_tr,~,~] = regression(trTarg, trOut);
+                Rsq_tr = 1 - sum((ytrain(tr.trainInd) - out_train(tr.trainInd)).^2)/sum((ytrain(tr.trainInd) - mean(ytrain(tr.trainInd))).^2);
+                Rsq_tt = 1 - sum((ytrain(tr.testInd) - out_train(tr.testInd)).^2)/sum((ytrain(tr.testInd) - mean(ytrain(tr.testInd))).^2);
+                %val_perf = tr.vperf(end);%mse(net,ytrain(tr.valInd),out_train(tr.valInd));
+                tr_perf = tr.perf(end);%mse(net,ytrain(tr.trainInd),out_train(tr.trainInd));
+                %bool_var = (val_perf>val_perf_reqd)||(reg_tr<reg_tr_reqd); % (tr_perf>tr_perf_reqd) Training parameter
+                bool_var = (Rsq_tt < Rsq_tt_reqd) || (Rsq_tr < Rsq_tr_reqd)
+                if tr.tperf(end)<tt_min %&& tr_perf<tr_perf_min
+                    tt_min = tt_perf;
+                    tr_perf_min = tr.perf(end);
+                    net_min = net;
+                    tr_min = tr;
+                end
+                if index_watch>=max_index % this is just for the case when the iterations do not converge even after 1000 runs ....
+                    ind_out(random_weights) = 1;
+                    break;
+                end
+                index_watch = index_watch +1;
+                
+            end
+            net_storage{random_weights} = net_min;
+            tr_storage{random_weights} = tr_min;
+            
+        end
+        
+        hidden_layer_size = hidden_layer_size + 1;
+    end
+    net_storage_complete{coeff_num} = net_storage;
+    tr_storage_complete{coeff_num} = tr_storage;
+    index_out_coeffs{coeff_num} = ind_out;
+    %test_err_complete{coeff_num} = test_err;
+end
+
+ngoodnets = zeros(1,num_coeffs);
+for i=1:num_coeffs
+    ngoodnets(i) = length(find(index_out_coeffs{i}==0)) ;
+end
+ngoodnets
+
+toc;
+save(strcat(lattice,'_layerfinal_resultsp1.mat'),'net_storage_complete','index_out_coeffs','tr_storage_complete')
